54fa687fcb8737ae6054515b2ebd9fc5ba7a4278
Kedar-Sahu <169009714+Kedar-Sahu@users.noreply.github.com>
2024-10-17 14:05:07 +0530

    Delete Employee-Management-System directory

--- Employee-Management-System/src/main/java/in/main/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package in.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.main.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
